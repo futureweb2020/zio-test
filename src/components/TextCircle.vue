fdd86790fe97c1fd75417ae2fe5f5580231346f9
Ramon Alberto <ra.hightech@aol.com>
2018-09-10 16:23:05 -0300

    fix position of text

--- src/components/TextCircle.vue
+++ src/components/TextCircle.vue
@@ -2,7 +2,7 @@
   <div>
     <svg :height="size" :width="size">
       <circle cx="50%" cy="50%" r="50%" :fill="bgColor" />
-      <text :x="centerX" :y="centerY">{{ text }}</text>
+      <text :x="centerX" :y="centerY" alignment-baseline="middle" text-anchor="middle">{{ text }}</text>
     </svg>
   </div>
 </template>
