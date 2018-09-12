bf6c20bad9bd577d38b6a3ab634ea2b095155266
Ramon Alberto <ra.hightech@aol.com>
2018-09-11 23:55:15 -0300

    modify x, y attribute values

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <input type="text" v-model="text"><br/><br/>
-    <TextCircle :text="text" :size="100" :centerX="50" :centerY="50" bgColor="green" />
-    <TextCircle :text="text" :size="150" :centerX="50" :centerY="75" bgColor="lightgreen" />
+    <TextCircle :text="text" :size="100" :centerX="200" :centerY="150" bgColor="green" />
+    <TextCircle :text="text" :size="150" :centerX="200" :centerY="300" bgColor="lightgreen" />
   </div>
 </template>
 
